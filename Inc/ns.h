123148e37f8223ac9d2fe2e63501f11a20e15a9e
th0truth <shiftrams@gmail.com>
2026-01-12 12:28:26 +0200

    feat: add typedef of unsigned long long and NUMERAL base representation

--- Inc/ns.h
+++ Inc/ns.h
@@ -3,17 +3,18 @@
 #ifndef NS_H
 #define NS_H
 
+typedef unsigned long long ull;
+typedef enum {
+  BIN = 2,
+  OCT = 8,
+  DEC = 10,
+  HEX = 16
+} NUMERAL; 
+
+ull to_decimal(const char* number, int base);
+void *to_binary(ull decimal);
+void *to_octal(ull decimal);
+
 int checkBin(const char* bin);
-long binToDec(const char* bin);
-long binToOctal(const char* bin);
 
-long hexToBin(const char* hex);
-long hexToDec(const char* hex);
-long hexToOct(const char* hex);
-
-long octToDec(const char *oct);
-long octToBin(const char* oct);
-
-char* decToBin(const char* dec);  
-char* decToOctal(const char* dec);
 #endif
