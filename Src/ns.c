413e6d730db75b475ac057b06021fc272f42932c
th0truth <shiftrams@gmail.com>
2026-01-12 12:29:35 +0200

    feat: implement better number conversion

--- Src/ns.c
+++ Src/ns.c
@@ -1,161 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+#include <errno.h>
 
 #include "utils.h"
 #include "ns.h"
 
-int checkBin(const char* bin)
-{
+// Convert string to a long number
+ull to_decimal(const char* number, int base) {
+  char *endptr;
+
+  ull value = strtoull(number, &endptr, base);
+  if (*endptr != '\0') {
+    fprintf(stderr, "Conversion error: invalid number\n");
+    exit(1);
+  }
+
+  if (errno == ERANGE) {
+    fprintf(stderr, "Number too large for uint64_t\n");
+    exit(1);
+  }
+
+  return value;
+}
+
+// Convert decimal to an octal string
+void *to_octal(ull decimal) {
+  if (decimal == 0) { return NULL; }
+  
+  char temp[128];
   int i = 0;
-  while (bin[i] != '\0') {
-    if (bin[i] != '0' && bin[i] != '1') {
-      return 0;
-    }
-    i++;
-  }
-  return 1;
-}
 
-
-long binToDec(const char* bin)
-{
-  int base = 1, reminder;
-  long decimal = 0; 
-  while (*bin != 0) {
-    decimal = decimal * 2 + (*bin - '0');
-    bin++;
-  }
-  return decimal;
-}
-
-
-long binToOctal(const char* bin)
-{
-  int octal = 0, base = 1, reminder;
-
-  // Binary to decimal
-  long decimal = binToDec(bin);
-
-  // Decimal to octal
-  while (decimal != 0) {
-    reminder = decimal % 8;
-    octal += reminder * base;
-    base *= 10;
+  while (decimal > 0) {
+    temp[i++] = (decimal % 8) + '0';
     decimal /= 8;
   }
+
+  char *octal = malloc(i + 1);
+  for (int j = 0; j < i; j++) {
+    octal[j] = temp[i - 1 - j];
+  }
+  octal[i] = '\0';
+
   return octal;
 }
 
-
-long hexToDec(const char* hex)
-{
-  long decimal = 0;
-  for(int base = 1, i = strlen(hex) - 1; i >= 0; i--) {
-    if(hex[i] >= '0' && hex[i] <= '9') {
-      decimal += (hex[i] - 48) * base;
-    } else if(hex[i] >= 'A' && hex[i] <= 'F') {
-      decimal += (hex[i] - 55) * base;
-    } else if(hex[i] >= 'a' && hex[i] <= 'f') {
-      decimal += (hex[i] - 87) * base;
-    }
-    base *= 16;
-  }
-  return decimal;
-}
-
-
-long hexToBin(const char* hex)
-{
-  long bin = 0ll;
-  for (int i = 0; hex[i] != '\0'; i++) {
-    bin *= 10000;
-    switch (hex[i])
-    {
-      case '0': bin += 0; break;
-      case '1': bin += 1; break;
-      case '2': bin += 10; break;
-      case '3': bin += 11; break;
-      case '4': bin += 100; break;
-      case '5': bin += 101; break;
-      case '6': bin += 110; break;
-      case '7': bin += 111; break;
-      case '8': bin += 1000; break;
-      case '9': bin += 1001; break;
-      case 'a': case 'A': bin += 1010; break;
-      case 'b': case 'B': bin += 1011; break;
-      case 'c': case 'C': bin += 1100; break;
-      case 'd': case 'D': bin += 1101; break;
-      case 'e': case 'E': bin += 1110; break;
-      case 'f': case 'F': bin += 1111; break;
-    }
-  }
-  return bin;
-}
-
-
-long hexToOct(const char* hex)
-{
-  int reminder, value;
-  long octal = 0ll;
-  
-  // Convert hex to bin, then bin to octal
-  long bin = hexToBin(hex); 
-  for (int i = 1; bin > 0; i *= 10)
-  {
-    reminder = bin % 1000;
-    switch (reminder)
-    {
-      case 0: value = 0; break;
-      case 1: value = 1; break;
-      case 10: value = 2; break;
-      case 11: value = 3; break;
-      case 100: value = 4; break;
-      case 101: value = 5; break;
-      case 110: value = 6; break;
-      case 111: value = 7; break;
-    }
-    octal = (value * i) + octal;
-    bin /= 1000;
-  }
-  return octal;
-}
-
-
-long octToDec(const char* oct)
-{
-  long decimal = 0;
-  long octal = ascii_to_integer(oct);
-  for (int i = 0; octal != 0; i++) {
-    decimal = decimal + (octal % 10) * pow(8, i);
-    octal /= 10;
-  }
-  return decimal;
-}
-
-
-long octToBin(const char* oct)
-{
-  long binary = 0;
-  
-  // Convert octal to decimal, then decimal to binary 
-  long decimal = octToDec(oct);
-  for (int i = 1; decimal != 0; i *= 10) {
-    binary += ((decimal % 2) * i);
-    decimal /= 2;
-  }
-  return binary;
-}
-
-
-char* decToBin(const char* dec)
-{
-  long decimal = ascii_to_integer(dec);
-  char* bin = malloc(65 * sizeof(char)); 
-  if (bin == NULL) {
-    return NULL;
-  }
+// Convert decimal to a binary string
+void* to_binary(ull decimal) {
+  char *bin = malloc(65 * sizeof(char));
+  if (bin == NULL) { return NULL; }
 
   int i = 0;
   if (decimal == 0) {
@@ -166,43 +59,26 @@ char* decToBin(const char* dec)
       decimal /= 2;
     }
   }
-   
   bin[i] = '\0';
 
-  char temp;
+  // Reverse the string
   for (int j = 0; j < i / 2; j++) {
-    temp = bin[j];
-    bin[j] = bin[i-j-1];
-    bin[i-j-1] = temp;
+    char temp = bin[j];
+    bin[j] = bin[i-1-j];
+    bin[i-1-j] = temp;
   }
+
   return bin;
 }
 
-
-char* decToOctal(const char* dec)
-{
-  long decimal = ascii_to_integer(dec);
-  char* octal = malloc(65 * sizeof(char));
-  if (octal == NULL) {
-    return NULL;
-  }
-
+// Validate binary string
+int checkBin(const char* bin) {
   int i = 0;
-  if (decimal == 0) {
-    octal[i++] = '0';
-  } else {
-    while (decimal > 0) {
-      octal[i++] = (decimal % 8) + '0';
-      decimal /= 8;
+  while (bin[i] != '\0') {
+    if (bin[i] != '0' && bin[i] != '1') {
+      return 0;
     }
+    i++;
   }
-  octal[i] = '\0';
-
-  char temp;
-  for (int j = 0; j < i / 2; j++) {
-    temp = octal[j];
-    octal[j] = octal[i-j-1];
-    octal[i-j-i] = temp;
-  }
-  return octal;
+  return 1;
 }
--- Src/utils.c
+++ Src/utils.c
@@ -38,71 +38,37 @@ int compare(const char* s1, const char* s2)
   }
 }
 
-
-long ascii_to_integer(const char* number)
-{
-  long n = 0;
-  for (int i = 0; number[i] != '\0'; i++) {
-    if (number[i] > '9' || number[i] < '0') {
-      return -1;
-    }
-    n = n * 10 + number[i] - '0';
-  }
-  return n;
-}
-
-
 int converter(char* res, const char* number, const char* format)
 {
+  ull decimal;
   if (number[0] == '0') {
-    if ((number[1] == 'b' || number[1] == 'B')) {
-      if (checkBin(number + 2) != 1) {
+    if (number[1] == 'b' || number[1] == 'B') {
+      if (checkBin(number+2) != 1) {
         return 1;
       }
-      if (compare(format, "dec") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", binToDec(number + 2));
-      } else if (compare(format, "oct") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", binToOctal(number + 2));
-      } else if (compare(format, "hex") == 0) {
-        snprintf(res, BUFFER_SIZE, "%X", binToDec(number + 2));
-      } else {
-        return 1;
-      }
+      decimal = to_decimal(number+2, BIN);
     } else if (number[1] == 'x' || number[1] == 'X') {
-      if (compare(format, "dec") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", hexToDec(number + 2));
-      } else if (compare(format, "bin") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", hexToBin(number + 2));
-      } else if (compare(format, "oct") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", hexToOct(number + 2));
-      } else {
-        return 1;
-      }
+      decimal = to_decimal(number+2, HEX);
     } else {
-      if (compare(format, "dec") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", octToDec(number + 1));
-      } else if (compare(format, "bin") == 0) {
-        snprintf(res, BUFFER_SIZE, "%ld", octToBin(number + 1));
-      } else if (compare(format, "hex") == 0) {
-        snprintf(res, BUFFER_SIZE, "%X", octToDec(number + 1));
-      } else {
-        return 1;
-      }
+      decimal = to_decimal(number+1, OCT);
     }
   } else {
-    if (compare(format, "bin") == 0) {
-      char* binary = decToBin(number);
-      snprintf(res, BUFFER_SIZE, "%s", binary);
-      free(binary);
-    } else if (compare(format, "oct") == 0) {
-      char *octal = decToOctal(number);
-      snprintf(res, BUFFER_SIZE, "%s", octal);
-      free(octal);
-    } else if (compare(format, "hex") == 0) {
-      snprintf(res, BUFFER_SIZE, "%X", ascii_to_integer(number));
-    } else {
-      return 1;
-    }
+    decimal = to_decimal(number, DEC);
   }
+
+  if (compare(format, "bin") == 0) {
+    char *binary = to_binary(decimal);
+    snprintf(res, BUFFER_SIZE, "%s", binary);
+    free(binary);
+  } else if (compare(format, "dec") == 0) {
+    snprintf(res, BUFFER_SIZE, "%llu", decimal);
+  } else if (compare(format, "oct") == 0) {
+    char *octal = to_octal(decimal);
+    snprintf(res, BUFFER_SIZE, "%s", octal);
+    free(octal);
+  } else if (compare(format, "hex") == 0) {
+    snprintf(res, BUFFER_SIZE, "%X", decimal);
+  }
+
   return 0;
 }
